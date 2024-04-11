a9ff3e3d1912e1983c08d8c5f609f398667940f3
Susana Cordoba <akimasc@gmail.com>
2024-04-11 12:55:52 +0200

    add persistance of searchview

--- app/src/main/java/com/susanadev/rickymorty/view/compose/NavigationComponent.kt
+++ app/src/main/java/com/susanadev/rickymorty/view/compose/NavigationComponent.kt
@@ -11,12 +11,9 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
